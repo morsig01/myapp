7b6af8cc61c445dfdada750399dd200c171cdd51
morsig01 <morgan.sigland@gmail.com>
2024-03-05 09:57:44 +0100

    init

---
+++ src/app/test/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+// import styles from "./page.module.css";
+
+export default function Home() {
+    let counter = 0;
+  return (
+    <div>
+      {
+        counter
+      }
+    </div>
+  );
+}
