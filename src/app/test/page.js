43b8c647b6866e9f7ce3113f99391a760bd9f335
morsig01 <morgan.sigland@gmail.com>
2024-03-05 10:57:15 +0100

    add blue square

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 // import styles from "./page.module.css";
+import "./page.css";
 
 export default function Home() {
-    let counter = 0;
   return (
-    <div>
-      {
-        counter
-      }
-    </div>
+    <><div className="box">
+        <h1>
+          hello
+        </h1>
+      </div></>
   );
 }
