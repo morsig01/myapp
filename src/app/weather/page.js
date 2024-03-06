45939febd8a62280b753c9ddad43a801154b993a
morsig01 <morgan.sigland@gmail.com>
2024-03-06 14:33:32 +0100

    add visuals and fix text

--- src/app/weather/page.js
+++ src/app/weather/page.js
@@ -13,9 +13,32 @@ console.log(res)
 export default async function Home() {
   const data = await getData();
  
-  return <main>
+  return <main> 
+    <div className="header">
+        <font size="42">Weather</font>
+    </div>
     {
-      data.properties.timeseries.map((time,index) => {return <div key={index}>{time.time}</div>})
+    data.properties.timeseries.map(
+      function(timeobj){
+        return(
+          <>
+          <div className="style">
+
+           <div className="bracket">
+              <h1>Dato</h1>
+              {timeobj.time}
+              <br></br>
+              <h1>Temperatur</h1>
+              {timeobj.data.instant.details.air_temperature}
+              <h1>Vindstyrke</h1>
+              {timeobj.data.instant.details.wind_speed}m/s
+              <br></br>
+            </div>
+          </div>
+          </>
+        )
+      }
+    )
     }
   </main>
 }
